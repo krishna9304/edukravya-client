5819a93c932d172fc0b326488ed7a0ef912e2ed3
Krishna Mahato <krishhtrishh9304@gmail.com>
2022-10-03 08:33:43 +0530

    ADD: Onboarding

---
+++ src/components/educatorUpdateFields.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface EducatorUpdateFieldsProps {}
+
+const EducatorUpdateFields: FunctionComponent<
+  EducatorUpdateFieldsProps
+> = () => {
+  return <div></div>;
+};
+
+export default EducatorUpdateFields;
